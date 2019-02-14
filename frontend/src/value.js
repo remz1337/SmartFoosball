3365e17a475b67e90bde60fd92e0008028b98c67
Rémi Bédard-Couture <remi.bc@outlook.com>
2019-02-14 01:58:45 -0500

    realtime bitcoin prices over websocket

---
+++ frontend/src/value.js
@@ -0,0 +1,8 @@
+import React from "react";
+//import { Line } from "react-chartjs-2";
+
+//const Value = props => <Line data={props.data} options={props.options} />;
+var Value = props; //props => {props.data};
+if ({props.data}) Value = {props.data};
+
+export default Value;
