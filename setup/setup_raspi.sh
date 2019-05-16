d3a13852f0a0575e3c7315eebe859fc7d8062625
Remi Bedard-Couture <remi.bc@outlook.com>
2019-05-16 13:25:34 -0400

    Comment on autostart sensors script and browser

--- setup/setup_raspi.sh
+++ setup/setup_raspi.sh
@@ -7,3 +7,11 @@ sudo apt-get -qy autoremove
 sudo apt-get -qy install xscreensaver
 
 #Then disable screen saver from Menu-->Preferences-->Display Mode
+
+#Deploy sensor script to crontab
+##crontab -e
+##@reboot python3 /home/pi/source/repos/SmartFoosball/sensors/foosball.py &
+
+#auto start chromium to localhost 
+##sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
+##/usr/bin/chromium-browser --kiosk --disable-restore-session-state http://localhost
